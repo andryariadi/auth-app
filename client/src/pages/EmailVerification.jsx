4bdf101f445a45b2aee58e467d998c2669ba4c5b
Andry Ariadi <andryariadi23@gmail.com>
2024-09-10 06:51:38 +0700

    styling email verification page

--- client/src/pages/EmailVerification.jsx
+++ client/src/pages/EmailVerification.jsx
@@ -1,5 +1,34 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 const EmailVerificationPage = () => {
-  return <div>EmailVerificationPage</div>;
+  const [code, setCode] = useState(["", "", "", "", "", ""]);
+  return (
+    <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className="w-full max-w-md bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
+      <div className="p-8 flex flex-col gap-6">
+        <h2 className="text-3xl font-bold text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">Verify Your Email</h2>
+
+        <span className="text-sm text-center text-gray-400">Enter the 6-digit code sent to your email address</span>
+
+        <form action="" className="flex flex-col gap-6">
+          <div className="bg-violt-500 bg-ros-500 flex items-center justify-between">
+            {code.map((digit, index) => (
+              <input key={index} type="text" maxLength="6" value={digit} className="size-12 text-center text-2xl font-bold bg-gray-700 text-white border-2 border-gray-600 rounded-lg focus:border-green-500 focus:outline-none" />
+            ))}
+          </div>
+
+          <motion.button
+            className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 transition-all duration-300"
+            whileHover={{ scale: 1.01 }}
+            whileTap={{ scale: 0.95 }}
+            type="submit"
+          >
+            Verify Email
+          </motion.button>
+        </form>
+      </div>
+    </motion.div>
+  );
 };
 
 export default EmailVerificationPage;
